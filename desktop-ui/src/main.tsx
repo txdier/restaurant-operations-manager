f829c18b0615b2ed13580c2d8069ba9bb33c79c0
Q <30993382+txdier@users.noreply.github.com>
2026-08-19 21:21:25 +0800

    Hide application content while locked

--- desktop-ui/src/main.tsx
+++ desktop-ui/src/main.tsx
@@ -13,5 +13,6 @@ import "../../app/admin-management.css";
 import "../../app/category-dialog.css";
 import "../../app/import-ui.css";
 import "../../app/status-filter.css";
+import "../../app/lock-screen.css";
 
 createRoot(document.getElementById("root")!).render(<React.StrictMode><Home/></React.StrictMode>);
