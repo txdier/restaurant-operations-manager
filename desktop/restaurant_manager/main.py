0b75058311d0484c4e2f41cdec76608e62f1801d
Q <30993382+txdier@users.noreply.github.com>
2026-08-27 20:17:10 +0800

    feat: install relational import routes

--- desktop/restaurant_manager/main.py
+++ desktop/restaurant_manager/main.py
@@ -9,6 +9,7 @@ from .server import start_server
 from .migration_routes import install_migration_routes
 from .core_data_routes import install_core_data_routes
 from .storage_export_routes_v6 import install_storage_export_routes
+from .import_routes_v6 import install_import_routes_v6
 from .version import APP_NAME, APP_VERSION
 
 
@@ -35,6 +36,7 @@ def main() -> int:
     install_migration_routes()
     install_core_data_routes()
     install_storage_export_routes()
+    install_import_routes_v6()
     logging.info("Starting %s %s", APP_NAME, APP_VERSION)
     from PyQt5.QtCore import QUrl
     from PyQt5.QtCore import Qt, QTimer
